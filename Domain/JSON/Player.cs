1d48d7ac67c52b2add35eed7105ef1b240193f81
Andreas Ommundsen <anthomm@live.com>
2020-09-10 22:39:27 +0200

    Refactor code, major speedup in get/mounts

---
+++ Domain/JSON/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Domain.JSON
+{
+    public class Player
+    {
+        [JsonPropertyName("mounts")]
+        public List<PlayerMount> Mounts { get; set; }
+    }
+
+    public class PlayerMount
+    {
+        [JsonPropertyName("mount")]
+        public PlayerMountInfo MountInfo { get; set; }
+    }
+
+    public class PlayerMountInfo
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
--- MountSnooper/Communication/Requester.cs
+++ MountSnooper/Communication/Requester.cs
@@ -1,9 +1,10 @@
 ﻿using Domain.DTOs;
 using Domain.Entities;
+using Domain.JSON;
 using MountSnooper.Authorization;
 using RestSharp;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 
 namespace MountSnooper.Communication
@@ -11,8 +12,7 @@ namespace MountSnooper.Communication
     public interface IRequester
     {
         IEnumerable<MountDTO> PlayerMounts(string name, string region, string realm);
-        public IEnumerable<Mount> AllMounts(string region="eu");
-        public string ImageURL(int mountId, string region = "eu");
+        IEnumerable<MountDTO> AllMountsAndURLs(string region = "eu");
     }
     public class Requester : IRequester
     {
@@ -38,71 +38,60 @@ namespace MountSnooper.Communication
 
             IRestResponse response = client.Execute(request);
 
-            OuterStable mounts = JsonSerializer.Deserialize<OuterStable>(response.Content);
+            Player player = JsonSerializer.Deserialize<Player>(response.Content);
             List<MountDTO> mountDTOs = new List<MountDTO>();
-            foreach (OuterMount m in mounts.Mounts)
+            foreach (PlayerMount mount in player.Mounts)
             {
-                mountDTOs.Add(new MountDTO() { Name = m.Mount.Name });
+                mountDTOs.Add(new MountDTO() { Name = mount.MountInfo.Name});
             }
 
             return mountDTOs;
         }
 
-        public IEnumerable<Mount> AllMounts(string region)
+        public IEnumerable<MountDTO> AllMountsAndURLs(string region = "eu")
         {
             RestClient client = new RestClient(
-                $"https://{region.ToLower()}.api.blizzard.com/data/wow/mount/index?namespace=static-{region.ToLower()}&locale={locale}");
+               $"https://{region}.api.blizzard.com/data/wow/search/mount?namespace=static-{region}&locale=en_GB&orderby=id&_page=1");
 
             RestRequest request = new RestRequest(Method.GET);
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("content-type", "application/x-www-form-urlencoded");
             request.AddHeader("authorization", $"Bearer {accessToken}");
 
-            IRestResponse response = client.Execute(request);
+            List<MountDTO> dtos = new List<MountDTO>();
 
-            Stable mounts = JsonSerializer.Deserialize<Stable>(response.Content);
+            int nextPage;
+            int pageCount;
+            do
+            {
+                IRestResponse response = client.Execute(request);
+                Mounts mounts = JsonSerializer.Deserialize<Mounts>(response.Content);
 
-            return mounts.Mounts;
+                foreach (MountResult result in mounts.Results)
+                {
+                    dtos.Add(
+                        new MountDTO()
+                        {
+                            Name = result.Data.Names.NameGB,
+                            ImageURL = URLString(result.Data.CreatureDisplay[0].Id)
+                        }
+                    );
+                }
+
+                nextPage = mounts.CurrentPage + 1;
+                pageCount = mounts.PageCount;
+
+                client.BaseUrl =
+                    new Uri($"https://{region}.api.blizzard.com/data/wow/search/mount?namespace=static-{region}&locale=en_GB&orderby=id&_page={nextPage}");
+
+            } while (nextPage <= pageCount);
+
+            return dtos;
         }
 
-        public string ImageURL(int mountId, string region)
+        private string URLString(int displayId, string region = "eu")
         {
-            int displayId = CreatureDisplayId(mountId, region);
-            return CreatureDisplayMediaURL(displayId, region);
-        }
-
-        private int CreatureDisplayId(int mountId, string region)
-        {
-            RestClient client = new RestClient(
-               $"https://{region}.api.blizzard.com/data/wow/mount/{mountId}?namespace=static-{region.ToLower()}&locale={locale}");
-
-            RestRequest request = new RestRequest(Method.GET);
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddHeader("authorization", $"Bearer {accessToken}");
-
-            IRestResponse response = client.Execute(request);
-
-            OuterCreatureDisplay ocd = JsonSerializer.Deserialize<OuterCreatureDisplay>(response.Content);
-
-            return ocd.Outer.ToList()[0].Id;
-        }
-
-        private string CreatureDisplayMediaURL(int displayId, string region)
-        {
-            RestClient client = new RestClient(
-               $"https://{region}.api.blizzard.com/data/wow/media/creature-display/{displayId}?namespace=static-{region.ToLower()}&locale={locale}");
-
-            RestRequest request = new RestRequest(Method.GET);
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddHeader("authorization", $"Bearer {accessToken}");
-
-            IRestResponse response = client.Execute(request);
-
-            OuterCreatureDisplayMedia ocdm = JsonSerializer.Deserialize<OuterCreatureDisplayMedia>(response.Content);
-
-            return ocdm.Outer.ToList()[0].ImageURL;
+            return $"https://render-{region}.worldofwarcraft.com/npcs/zoom/creature-display-{displayId}.jpg";
         }
     }
 }
